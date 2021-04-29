c75cd791aa18d4ea7ae04bae39cce76b4f627c84
Dimitar Zabaznoski <dimitar.zabaznoski@webfactory.mk>
2021-04-29 02:27:03 +0200

    Fix formatting

--- lib/stub_storage.dart
+++ lib/stub_storage.dart
@@ -3,7 +3,6 @@ import 'package:single_item_storage/storage.dart';
 /// Const implementation that won't store any items.
 /// Useful for default/null values.
 class StubStorage<E> implements Storage<E> {
-
   const StubStorage();
 
   @override
